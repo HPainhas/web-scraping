1813475e6a63b47502c68bab34eda52286ce37d4
Henrique Painhas <hpainhas@hotmail.com>
2018-10-02 18:31:03 -0400

    Update webscraper.rb

--- webscraper.rb
+++ webscraper.rb
@@ -8,33 +8,28 @@ require_relative "article"
 def articles_percentage (articles)
   
   # declare variables to keep track of str1 and str2 appearances, and total number of articles
-  OSU_count = 0
-  President_count = 0
-  Article_total = 0
+  osu_count = 0
+  president_count = 0
+  article_total = 0
   str1 = "Ohio State"
   str2 = "President Michael V. Drake"
   
   articles.each do |article|
-        # if article includes "Ohio State" in its title, add one to OSU_count
+        # if article includes "Ohio State" in its title, add one to osu_count
         if article.title.to_s.include? str1
-              OSU_count += 1
-        # if article includes "President Michael V. Drake" in its title, add one to President_count
+              osu_count += 1
+        # if article includes "President Michael V. Drake" in its title, add one to president_count
         elsif article.title.to_s.include? str2
-              President_count += 1
+              president_count += 1
         end
         # add one to total number of articles in news.osu.edu
-        Article_total += 1
+        article_total += 1
   end
   
-  OSU_percentage = (OSU_count.to_f / Article_total.to_f) * 100.to_f
-  President_percentage = (President_count.to_f / Article_total.to_f) * 100.to_f
+  osu_percentage = (osu_count.to_f / Article_total.to_f) * 100.to_f
+  president_percentage = (president_count.to_f / article_total.to_f) * 100.to_f
   
-  puts "\n#{OSU_percentage}% of articles have \"Ohio State\" in their title"
-  puts "\n#{President_percentage}% of articles have \"President Michael V. Drake\" in their title"
+  puts "\n#{osu_percentage}% of articles have \"Ohio State\" in their title"
+  puts "\n#{president_percentage}% of articles have \"President Michael V. Drake\" in their title"
 end
-  
-  
-  
-  
-  
   
