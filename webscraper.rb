fa3b3e47a328fc17f28428971b25fc17d9b5bcaa
Henrique Painhas <hpainhas@hotmail.com>
2018-10-02 18:29:42 -0400

    Update webscraper.rb

--- webscraper.rb
+++ webscraper.rb
@@ -10,13 +10,9 @@ def articles_percentage (articles)
   # declare variables to keep track of str1 and str2 appearances, and total number of articles
   OSU_count = 0
   President_count = 0
-  total = 0
+  Article_total = 0
   str1 = "Ohio State"
   str2 = "President Michael V. Drake"
-  
-  
-  agent = Mechanize.new
-  page = @articles.get(
   
   articles.each do |article|
         # if article includes "Ohio State" in its title, add one to OSU_count
@@ -27,8 +23,14 @@ def articles_percentage (articles)
               President_count += 1
         end
         # add one to total number of articles in news.osu.edu
-        total += 1
+        Article_total += 1
   end
+  
+  OSU_percentage = (OSU_count.to_f / Article_total.to_f) * 100.to_f
+  President_percentage = (President_count.to_f / Article_total.to_f) * 100.to_f
+  
+  puts "\n#{OSU_percentage}% of articles have \"Ohio State\" in their title"
+  puts "\n#{President_percentage}% of articles have \"President Michael V. Drake\" in their title"
 end
   
   
